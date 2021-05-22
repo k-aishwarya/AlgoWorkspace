3f357ef0d3c6d6329f255d8c55106af519e37795
kaishwarya <kumari_aishwarya@intuit.com>
2021-05-22 20:47:12 +0530

    abstraction

--- src/Abstraction/Animal.java
+++ src/Abstraction/Animal.java
@@ -2,7 +2,11 @@ package Abstraction;
 
 public abstract class Animal {
     public int legs=8;
+    private String species = "any";
     public static String name= "I am an Animal";
+    private void mySpecies(){
+        System.out.println(species);
+    }
     public abstract void run();
     public abstract void eat();
 }
