7df862c7180b6b8f28d7a1fa22ae5c91332920a6
k.aishwarya15@gmail.com <k.aishwarya15@gmail.com>
2021-05-24 22:47:57 +0530

    improve

--- src/educative/Array/Challenge4.java
+++ src/educative/Array/Challenge4.java
@@ -9,20 +9,25 @@ public class Challenge4 {
         int [] result = new int[n];
 
         int prod=1;
-        boolean zero_flag=false;
+        int zero_count = 0;
         for(int i=0; i<n; i++){
-            if(arr[i]==0) zero_flag=true;
+            if(arr[i]==0) zero_count++;
             else prod*=arr[i];
         }
         for(int i=0; i<n; i++){
-            if(arr[i]==0) result[i] = prod;
-            else result[i] = zero_flag==true? 0: prod/arr[i];
+            if(arr[i]==0){
+                if(zero_count==1)
+                    result[i] = prod;
+                else
+                    result[i] = 0;
+            }
+            else result[i] = zero_count>0? 0: prod/arr[i];
         }
         return result;
     }
 
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4};
+        int[] arr = {1,2,3,4,0,0};
         Arrays.stream(Challenge4.findProduct(arr)).forEach(System.out::println);
     }
 }
