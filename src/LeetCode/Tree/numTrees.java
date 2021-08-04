e58983d29555f072d9fd1747b2520cd3ac8cad74
k.aishwarya15@gmail.com <k.aishwarya15@gmail.com>
2021-08-04 21:25:43 +0530

    numTrees

---
+++ src/LeetCode/Tree/numTrees.java
@@ -0,0 +1,23 @@
+package LeetCode.Tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class numTrees {
+    public static int numTrees(int n) {
+        if(n<1) return 0;
+        int[] trees = new int[n+1];
+        trees[0]=1;
+
+        for(int i=1; i<=n; i++){
+            for(int j=0; j<i; j++){
+                trees[i] += trees[j] * trees[i-j-1];
+            }
+        }
+        return trees[n];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(numTrees(3));
+    }
+}
