e0cfc2a086e87138380ad611701e88b7d7292323
k.aishwarya15@gmail.com <k.aishwarya15@gmail.com>
2021-08-07 17:52:14 +0530

    BSTIterator

---
+++ src/LeetCode/Tree/BSTIterator.java
@@ -0,0 +1,43 @@
+package LeetCode.Tree;
+
+import LeetCode.Tree.common.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BSTIterator {
+
+    private TreeNode root;
+    private int pointer=-1;
+    private List<TreeNode> inOrder;
+    private int count=0;
+
+    public BSTIterator(TreeNode root) {
+        this.root = root;
+        if(root!=null) {
+            inOrder= new ArrayList<>();
+            inOrder(root);
+            pointer=0;
+        }
+    }
+
+    public int next() {
+        if(pointer>=0 && pointer<count){
+            return this.inOrder.get(pointer++).val;
+        }
+        else return 0;
+    }
+
+    private void inOrder(TreeNode root){
+        if(root!=null){
+            inOrder(root.left);
+            this.inOrder.add(root);
+            count++;
+            inOrder(root.right);
+        }
+    }
+
+    public boolean hasNext() {
+        return pointer<count;
+    }
+}
