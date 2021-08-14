913a592f754fa1507e38bb04caf6a23753ef2f9b
k.aishwarya15@gmail.com <k.aishwarya15@gmail.com>
2021-08-14 18:41:49 +0530

    convertBST

---
+++ src/LeetCode/Tree/convertBST.java
@@ -0,0 +1,18 @@
+package LeetCode.Tree;
+
+import LeetCode.Tree.common.TreeNode;
+
+public class convertBST {
+
+    int sum = 0;
+
+    public TreeNode convertBST(TreeNode root) {
+        if(root!=null){
+            convertBST(root.right);
+            sum+=root.val;
+            root.val=sum;
+            convertBST(root.left);
+        }
+        return root;
+    }
+}
